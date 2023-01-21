fdee6a55c0acba3f54f75c98838a8ed607586014
Mirisan100 <mirisan003@gmail.com>
2023-01-21 18:20:26 -0500

    Trying

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
-// Listen for submit
+function calculate() {
+    var loanAmount = document.getElementById("loan-amount").value;
+    var interestRate = document.getElementById("interest-rate").value;
+    var loanTerm = document.getElementById("loan-term").value;
+    // perform calculations here (example calculation is for demonstration purposes only)
+    var monthlyPayment = (loanAmount * interestRate) / (12 * (1 - Math.pow(1 + interestRate, -loanTerm)));
 
-document.getElementById("loan-form").addEventListener("submit", computeResults);
-
-function computeResults(e) {
-  // UI
-
-  const UIamount = document.getElementById("amount").value;
-  const UIinterest = document.getElementById("interest").value;
-  const UIyears = document.getElementById("years").value;
-
-  // Calculate
-
-  const principal = parseFloat(UIamount);
-  const CalculateInterest = parseFloat(UIinterest) / 100 / 12;
-  const calculatedPayments = parseFloat(UIyears) * 12;
-
-  //Compute monthly Payment
-
-  const x = Math.pow(1 + CalculateInterest, calculatedPayments);
-  const monthly = (principal * x * CalculateInterest) / (x - 1);
-  const monthlyPayment = monthly.toFixed(2);
-
-  //Compute Interest
-
-  const totalInterest = (monthly * calculatedPayments - principal).toFixed(2);
-
-  //Compute Total Payment
-
-  const totalPayment = (monthly * calculatedPayments).toFixed(2);
-
-  //Show results
-
-  document.getElementById("monthlyPayment").innerHTML = "$" + monthlyPayment;
-
-  document.getElementById("totalInterest").innerHTML = "%" + totalInterest;
-
-  document.getElementById("totalPayment").innerHTML = "$" + totalPayment;
-
-  e.preventDefault();
+    // output the results to the "output" element
+    var output = document.getElementById("output");
+    output.innerHTML = "Monthly Payment: $" + monthlyPayment;
 }
