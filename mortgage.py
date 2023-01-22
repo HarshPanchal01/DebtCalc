34e723cf1240900bb25f91b18339928699071c74
James Mata <jamesmata556@gmail.com>
2023-01-21 21:47:57 -0500

    done

---
+++ mortgage.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+def find_canada(countries):
+    for country in countries:
+        if 'Canada' in country:
+            return country
+
+def mortgage(debt):
+    api_url = 'https://api.api-ninjas.com/v1/interestrate'
+    response = requests.get(api_url, headers={'X-Api-Key': 'lzOCBfqKWqzS3OjFEVo38A==VtpOgKNNypCAecv5'})
+    if response.status_code == requests.codes.ok:
+        response=response.text.split("{")
+        canada=find_canada(response)
+        canada=canada.split(",")            ##please find a better way to clean this data
+        canada=canada[2]
+        canada=canada[-4:]
+        canada=float(canada)
+        canada=(canada/100)
+        
+        debt_over_time=[debt*(canada+1),debt*(canada*3+1),debt*(canada*5+1),debt*(canada*10+1)]
+
+        return debt_over_time
+
+
+    else:
+        print("Error:", response.status_code, response.text)
