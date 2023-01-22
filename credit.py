b909566405fccbdc65cf11e4a4f65016eaf4b54f
Andres Cardoso <andres.cardoso@ontariotechu.net>
2023-01-21 22:39:38 -0500

    For James

--- credit.py
+++ credit.py
@@ -1,11 +1,5 @@
 def Credit(balance):
-    intrest = [1, 3, 5, 10]
-    for i in range(len(intrest)):
-        credit = 0
-        for j in range(intrest[i]):
-            credit = credit + balance
-        credit = credit + credit*0.1999
-        intrest[i] = credit
-    return intrest
+    balance = balance + balance*0.1999
+    return balance
 
 print(Credit(20000))
