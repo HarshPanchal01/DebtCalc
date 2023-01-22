6a54df6d66b8bd8a341d063bed8f88c7402776a8
AndrewCardoso <123257055+AndrewCardoso@users.noreply.github.com>
2023-01-21 23:03:25 -0500

    Update credit.py
    Done

--- credit.py
+++ credit.py
@@ -14,14 +14,9 @@ credit(5)
 
 =======
 def Credit(balance):
-    intrest = [1, 3, 5, 10]
-    for i in range(len(intrest)):
-        credit = 0
-        for j in range(intrest[i]):
-            credit = credit + balance
-        credit = credit + credit*0.1999
-        intrest[i] = credit
-    return intrest
+    def Credit(balance):
+    balance = balance + balance*0.1999
+    return balance
 
 print(Credit(20000))
 >>>>>>> 176bbb1d6e3432b5e5bb4fb3d0cd9906c23682b2
